4a3c4fd4722cd694d5cb446bfb28e2352e34a878
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2020-04-27 12:09:52 -0700

    Specifying options such as the title to render in the header

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,38 +11,29 @@ function HomeScreen() {
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{ title: 'Overview' }} 
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// // comment
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Rosa's app works!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
