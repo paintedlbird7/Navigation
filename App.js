b3c2cc9fbf9f2f393f64cf70fa015d8898740b5f
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2020-04-27 11:31:16 -0700

    Home Screen working

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-// comment
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Rosa's app works!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// // comment
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Rosa's app works!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
