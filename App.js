e94cbc618f9ca5db0199ef4f0c2af167e8be4688
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2020-04-27 12:22:45 -0700

    Moving between screens:  Home & Details

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
@@ -25,12 +29,19 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
+        
+      {/* <Stack.Screen name="Home">
+  {props => <HomeScreen {...props} extraData={someData} />}
+</Stack.Screen> */}
+
         <Stack.Screen 
         name="Home" 
         component={HomeScreen}
         options={{ title: 'Overview' }} 
         />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen 
+        name="Details" 
+        component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
